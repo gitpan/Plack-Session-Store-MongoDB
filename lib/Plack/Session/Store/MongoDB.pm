d1e324b81ea223dc566492d341959ae1aa01d3b8
עידו פרלמוטר (Ido Perlmuter) <ido@ido50.net>
2010-09-15 15:04:49 +0000

    Import of IDOPEREL/Plack-Session-Store-MongoDB-0.2 from CPAN.
    gitpan-cpan-distribution: Plack-Session-Store-MongoDB
    gitpan-cpan-version:      0.2
    gitpan-cpan-path:         IDOPEREL/Plack-Session-Store-MongoDB-0.2.tar.gz
    gitpan-cpan-author:       IDOPEREL
    gitpan-cpan-maturity:     released

--- lib/Plack/Session/Store/MongoDB.pm
+++ lib/Plack/Session/Store/MongoDB.pm
@@ -1,6 +1,6 @@
 package Plack::Session::Store::MongoDB;
 BEGIN {
-  $Plack::Session::Store::MongoDB::VERSION = '0.1';
+  $Plack::Session::Store::MongoDB::VERSION = '0.2';
 }
 
 use warnings;
@@ -17,7 +17,7 @@ Plack::Session::Store::MongoDB - MongoDB based session store for Plack apps.
 
 =head1 VERSION
 
-version 0.1
+version 0.2
 
 =head1 SYNOPSIS
 
